1b6afe1d708e56253a2961d65e0c50e198a8f926
lotki13 <dkondrasov309@gmail.com>
2026-06-09 08:14:04 +0300

    add progression game

---
+++ src/games/progression.js
@@ -0,0 +1,42 @@
+import runGame from '../index.js';
+
+const description = 'What number is missing in the progression?';
+
+const progressionLength = 10;
+
+const getRandomNumber = (min, max) => (
+  Math.floor(Math.random() * (max - min + 1)) + min
+);
+
+const makeProgression = (start, step, length) => {
+  const progression = [];
+
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + i * step);
+  }
+
+  return progression;
+};
+
+const getRoundData = () => {
+  const start = getRandomNumber(1, 20);
+  const step = getRandomNumber(1, 10);
+
+  const progression = makeProgression(start, step, progressionLength);
+
+  const hiddenIndex = getRandomNumber(0, progressionLength - 1);
+
+  const correctAnswer = String(progression[hiddenIndex]);
+
+  progression[hiddenIndex] = '..';
+
+  const question = progression.join(' ');
+
+  return [question, correctAnswer];
+};
+
+const progressionGame = () => {
+  runGame(description, getRoundData);
+};
+
+export default progressionGame;
